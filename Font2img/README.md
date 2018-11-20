fd013627add9b77d5e74f5fe6d8949f5f1c4b338
hangyhan <hangyhan@mail.ustc.edu.cn>
2018-11-20 16:32:21 +0800

    Add font2img code

---
+++ Font2img/README.md
@@ -0,0 +1,16 @@
+###This folder contains the code of transfering fonts in ttf/tcc form to PNG form.
+
+
+To run this code, enter the following lines in the terminal:
+>python font2img.py 
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;     --font=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;       --ch=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        --font_size=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        --canvas_size=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        --x_offset=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        --y_offset=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        --gray=
+> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   --output_dir=     
+
+Here are some images generated from this code:
+![image](./imgs/Xnip2018-11-20_16-27-44.jpg)
--- Font2img/font2img.py
+++ Font2img/font2img.py
@@ -1,18 +1,27 @@
+# encoding=UTF-8
 import argparse
 import os
+import codecs
+
 
 from PIL import Image, ImageDraw, ImageFont
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--font', type=str, default='font/simkai.ttf', help='input the font name')
-    parser.add_argument('--ch', type=str, default='田', help='please input a chinese character')
-    parser.add_argument('--font_size', type=int, default=180, help="the size of the font")
-    parser.add_argument('--canvas_size', type=int, default=200, help='the size of canvas')
+    parser.add_argument('--font', type=str,
+                        default='font/simkai.ttf', help='input the font name')
+    parser.add_argument('--ch', type=str, default='田',
+                        help='please input a chinese character')
+    parser.add_argument('--font_size', type=int,
+                        default=120, help="the size of the font")
+    parser.add_argument('--canvas_size', type=int,
+                        default=128, help='the size of canvas')
     parser.add_argument('--x_offset', type=int, default=0, help='x offset')
     parser.add_argument('--y_offset', type=int, default=0, help='y offset')
-    parser.add_argument('--gray', action='store_true', help='output a gray image instead of a color image')
-    parser.add_argument('--output_dir', type=str, default='./', help='specify the output directory')
+    parser.add_argument('--gray', action='store_true',
+                        help='output a gray image instead of a color image')
+    parser.add_argument('--output_dir', type=str, default='./',
+                        help='specify the output directory')
 
     FLAGS = parser.parse_args()
     font = FLAGS.font
@@ -22,24 +31,27 @@ if __name__ == '__main__':
     y_offset = FLAGS.y_offset
     gray = FLAGS.gray
     output_dir = './img_lib/' + font.split('/')[-1].split('.')[0]
+    # print(output_dir)
 
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-
-    #load the common words
+    # load the common words
     ch = []
-    f = open( 'commom_words.txt', 'r', encoding='utf-8' )
+    f = codecs.open('commom_words.txt', 'r', encoding='utf-8')
     for word in f.read():
         ch.append(word)
+        # print(word)
     f.close()
 
     font = ImageFont.truetype(font, font_size)
 
     for word in ch:
+        # print(word)
         image = Image.new("RGB", (canvas_size, canvas_size), (255, 255, 255))
         draw = ImageDraw.Draw(image)
         draw.text((x_offset, y_offset), word, (0, 0, 0), font=font)
         if gray:
-            image = image.convert('L')
-        image.save(f'{output_dir}/{word}.png')
+            image = image.convert('1')
+        save_dir = output_dir + '/' + word + '.png'
+        image.save(save_dir)
