7bee07936ca8925f09196d4eef6554253ff97a52
hangyhan <hangyhan@mail.ustc.edu.cn>
2018-12-09 14:16:01 +0800

    Update font2img.py

--- Font2img/font2img.py
+++ Font2img/font2img.py
@@ -8,14 +8,15 @@ from PIL import Image, ImageDraw, ImageFont
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    #font要用绝对路径
     parser.add_argument('--font', type=str,
                         default='font/simkai.ttf', help='input the font name')
     parser.add_argument('--ch', type=str, default='田',
                         help='please input a chinese character')
     parser.add_argument('--font_size', type=int,
-                        default=120, help="the size of the font")
+                        default=60, help="the size of the font")
     parser.add_argument('--canvas_size', type=int,
-                        default=128, help='the size of canvas')
+                        default=64, help='the size of canvas')
     parser.add_argument('--x_offset', type=int, default=0, help='x offset')
     parser.add_argument('--y_offset', type=int, default=0, help='y offset')
     parser.add_argument('--gray', action='store_true',
